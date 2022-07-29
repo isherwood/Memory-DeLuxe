c29eb68dbb4a2daf6a591f0cc39d9c37d3c356b8
Clint Buhs <clint.buhs@stearnsbank.com>
2022-07-29 13:08:03 -0500

    Random checkpoint

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     const [secondSquare, setSecondSquare] = useState({});
     const [gridDimensions, setGridDimensions] = useState([0, 0]);
     const [gameLocked, setGameLocked] = useState(false);
+    const [players, setPlayers] = useState([]);
+    const [currentPlayer, setCurrentPlayer] = useState();
 
     const shuffle = array => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -64,14 +66,29 @@ function App() {
 
         // reset grid to current dimensions to fetch new images
         const currentGridDimensions = gridDimensions;
-        setGridDimensions([0,0]);
+        setGridDimensions([0, 0]);
         setTimeout(() => {
             setGridDimensions(currentGridDimensions);
         });
+
+        // set the first player as active
+        if (players.length) {
+            const tempPlayers = [...players];
+            tempPlayers[0].active = true;
+            setPlayers(tempPlayers);
+        }
     }
 
     const handleEndButtonClick = () => {
         setGameLocked(false);
+    }
+
+    const handleAddPlayer = (name) => {
+        setPlayers(players => [...players, {name: name}]);
+    }
+
+    const handleRemovePlayer = name => {
+        setPlayers(players.filter(player => player.name !== name));
     }
 
     useEffect(() => {
@@ -101,8 +118,20 @@ function App() {
             setSquares(current => current.map(obj => {
                 return {...obj, shown: false}
             }));
+
+            // increment current player score
+            setPlayers(current => current.map(obj => {
+                if (obj.name === currentPlayer['name']) {
+                    return {...obj, score: currentPlayer['score'] + 1}
+                }
+
+                return obj;
+            }));
+        } else {
+
+            // move to the next player
         }
-    }, [firstSquare, secondSquare]);
+    }, [currentPlayer, firstSquare, secondSquare]);
 
     useEffect(() => {
         const getSquares = count => {
@@ -137,6 +166,14 @@ function App() {
         }
     }, [gridDimensions]);
 
+    useEffect(() => {
+        const activePlayer = players.filter(player => player.active)[0];
+
+        if (activePlayer) {
+            setCurrentPlayer(activePlayer.name);
+        }
+    }, [players])
+
     return (
         <Container fluid className={(gameLocked ? 'game-locked' : '')}>
             <Row>
@@ -144,6 +181,9 @@ function App() {
                     onSquareCountChange={(grid) => setGridDimensions(JSON.parse(grid))}
                     gameLocked={gameLocked}
                     gridDimensions={gridDimensions}
+                    players={players}
+                    addPlayer={handleAddPlayer}
+                    removePlayer={handleRemovePlayer}
                     onStartButtonClick={handleStartButtonClick}
                     onEndButtonClick={handleEndButtonClick}/>
             </Row>
--- src/components/GameConfig/GameConfig.jsx
+++ src/components/GameConfig/GameConfig.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import {Form} from 'react-bootstrap';
+import React, {useState} from "react";
+import {Button, Col, Form, Row} from 'react-bootstrap';
 import ButtonWithConfirm from "../ButtonWithConfirm/ButtonWithConfirm";
 
 const GameConfig = props => {
+    const [showPlayers, setShowPlayers] = useState(false);
+    const [name, setName] = useState('');
+    const nameInput = React.createRef();
+
     const handleCountChange = e => {
         props.onSquareCountChange(e.currentTarget.value);
     }
@@ -11,33 +15,103 @@ const GameConfig = props => {
         [3, 2], [4, 3], [5, 4], [6, 5], [7, 6], [8, 7]
     ]
 
+    const handleShowPlayers = () => {
+        setShowPlayers(!showPlayers);
+
+        // setTimeout(() => {
+        //     nameInput.current.focus();
+        // });
+    }
+
+    const handleAddPlayer = () => {
+        if (name && !props.players.includes(name)) {
+            props.addPlayer(name);
+            setName('');
+            nameInput.current.focus();
+        }
+    }
+
+    const handleNameInputKeyup = event => {
+        if (event.key === 'Enter') {
+            handleAddPlayer();
+        }
+    }
+
     return (
-        <div id='gameConfig' className='d-flex justify-content-between mt-3'>
-            <div className='form-floating'>
-                <Form.Select id="squareCount" className="w-auto" onChange={handleCountChange}
-                             disabled={props.gameLocked}>
-                    <option value='[0,0]'>Select a square count</option>
-                    {gridOptions.map(option => (
-                        <option value={'[' + option[0] + ',' + option[1] + ']'}
-                                key={option[0] + option[1]}>{option[0] * option[1]}</option>
+        <>
+            <div className='d-flex align-items-center mt-3'>
+                {!props.gameLocked &&
+                    <>
+                        <div className='form-floating'>
+                            <Form.Select id="squareCount" className="w-auto"
+                                         onChange={handleCountChange}
+                                         disabled={props.gameLocked}>
+                                <option value='[0,0]'>Select a square count</option>
+                                {gridOptions.map(option => (
+                                    <option value={'[' + option[0] + ',' + option[1] + ']'}
+                                            key={option[0] + option[1]}>{option[0] * option[1]}</option>
+                                ))}
+                            </Form.Select>
+                            <Form.Label htmlFor="squareCount">Square Count</Form.Label>
+                        </div>
+
+                        <Button variant='primary' className='btn-xl ms-2'
+                                onClick={handleShowPlayers}>Players (optional)</Button>
+                    </>
+                }
+
+                {props.gameLocked && props.currentPlayer &&
+                    <div className='lead'>
+                        <p>Current player: <b>{props.currentPlayer}</b></p>
+
+                        <p>Scores: </p>
+                    </div>
+                }
+
+                <div className='ms-auto'>
+                    <button className={'btn btn-primary btn-xl' + (props.gameLocked ? ' d-none' : '')}
+                            onClick={props.onStartButtonClick}
+                            disabled={props.gridDimensions[0] === 0}>
+                        Start Game
+                    </button>
+
+                    <ButtonWithConfirm variant="danger" size="xl" value='End Game'
+                                       classes={(!props.gameLocked ? ' d-none' : '')}
+                                       modalBody={<>Are you sure you want to end the game?</>}
+                                       onYes={() => props.onEndButtonClick()}/>
+                </div>
+            </div>
+
+            {showPlayers && !props.gameLocked &&
+                <div className='mt-3'>
+                    <p>Optionally add players to track game order and score. Names must be unique.</p>
+
+                    <Row>
+                        <Col>
+                            <Form.Control type='text' placeholder='Enter player name' value={name}
+                                          onChange={event => setName(event.target.value)}
+                                          onKeyUp={handleNameInputKeyup}
+                                          ref={nameInput}/>
+                        </Col>
+
+                        <Col>
+                            <Button variant='secondary'
+                                    onClick={handleAddPlayer}><b>+</b></Button>
+                        </Col>
+                    </Row>
+
+                    {props.players.map(player => (
+                        <Row className='mt-2 px-2' key={player.name}>
+                            <Col>{player.name}</Col>
+
+                            <Col><Button variant='secondary'
+                                         onClick={() => props.removePlayer(player.name)}><b>&ndash;</b></Button>
+                            </Col>
+                        </Row>
                     ))}
-                </Form.Select>
-                <Form.Label htmlFor="squareCount">Square Count</Form.Label>
-            </div>
-
-            <div>
-                <button className={'btn btn-primary btn-xl' + (props.gameLocked ? ' d-none' : '')}
-                        onClick={props.onStartButtonClick}
-                        disabled={props.gridDimensions[0] === 0}>
-                    Start Game
-                </button>
-
-                <ButtonWithConfirm variant="danger" size="xl" value='End Game'
-                                   classes={(!props.gameLocked ? ' d-none' : '')}
-                                   modalBody={<>Are you sure you want to end the game?</>}
-                                   onYes={() => props.onEndButtonClick()}/>
-            </div>
-        </div>
+                </div>
+            }
+        </>
     );
 }
 
