e2b843484a07bb0cc97b4bde9630f14f063ea72b
Clint Buhs <clint@planetbuhs.net>
2022-09-13 08:45:54 -0500

    Handling failed image URLs

--- src/components/GameTile/GameTile.jsx
+++ src/components/GameTile/GameTile.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 import './styles.css';
+import warningTriangle from "../../images/warning-triangle.png";
 
 const GameTile = props => {
-    const innerStyles = {
-        backgroundImage: 'url(' + props.tile.url + ')'
+    const innerStyles = () => {
+        if (props.tile.error) {
+            return {
+                backgroundImage: 'url(' + warningTriangle + ')',
+                backgroundSize: 'contain'
+            }
+        } else {
+            return {backgroundImage: 'url(' + props.tile.url + ')'}
+        }
     }
 
     const handleTileClick = (event, tile) => {
@@ -15,8 +23,11 @@ const GameTile = props => {
         <div className='tile-outer'>
             <div
                 className={'tile-inner h-100' + (props.tile.shown ? ' shown' : '') + (props.tile.matched ? ' matched' : '')}
-                style={innerStyles}
+                style={innerStyles()}
                 onClick={event => handleTileClick(event, props.tile)}></div>
+
+            <img className="visually-hidden" aria-hidden="true" src={props.tile.url}
+                 onError={() => props.tile.error = true} alt="hidden dummy image to test URL"/>
         </div>
     );
 }
